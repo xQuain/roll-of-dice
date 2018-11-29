1d5d650c5a7239352ab019d03be8c6ace7a7b396
xQuain <40060776+xQuain@users.noreply.github.com>
2018-11-29 12:21:58 +0100

    Add files via upload

---
+++ roll-of-dice.py
@@ -0,0 +1,13 @@
+#Roll of dice
+#Generating random numbers
+
+import random
+
+#Generate random number between 1-6
+
+roll = random.randint(1,6)
+roll2 = random.randint(1,6)
+
+total = roll + roll2
+
+print("You rolled a", roll, "and a", roll2, "for a total of", total)
